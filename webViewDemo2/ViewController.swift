c307f22ef928c6b17aed2bd10c552c4ef12d772f
Clinton de Sá Barreto Maciel <clintonsbm@gmail.com>
2016-07-28 09:07:51 -0300

    Initial Commit

---
+++ webViewDemo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  webViewDemo2
+//
+//  Created by Clinton de Sá Barreto Maciel on 28/07/16.
+//  Copyright © 2016 Clinton de Sá Barreto Maciel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
